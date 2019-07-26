f3a3eacbbcb8543f8c93e7624dc550a4e9b45f86
Massimiliano <massimiliano.giovagnoli@innoteam.it>
2019-07-26 13:29:25 +0200

    Split network from main resources

--- aws/provisioning/main.tf
+++ aws/provisioning/main.tf
@@ -1,14 +1,3 @@
-# VPC
-
-module "vpc" {
-  source = "github.com/maxgio92/terraform-aws-vpc"
-
-  prefix_name          = "${var.env}"
-  vpc_cidr             = "${var.vpc_cidr}"
-  public_subnet_count  = "${var.public_subnet_count}"
-  private_subnet_count = "${var.private_subnet_count}"
-}
-
 # AMI
 
 data "aws_caller_identity" "current" {}
